bdca364b78e4e97f531498025300e1600a68bc71
cad9wo-max <75642356+cad9wo-max@users.noreply.github.com>
2022-03-15 12:55:25 +0000

    Completed Exercise002 method implementations isFromManchester, canWatchFilm )

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -1,15 +1,60 @@
 package com.techreturners.exercise002;
 
+import java.util.Objects;
+
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
-        // Add your code here
-        return false;
+    public boolean isFromManchester(Person person) {
+        return Objects.equals(person.getCity(), "Manchester");
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
-        // Add your code here
-        return false;
+    public boolean canWatchFilm(Person person, int ageLimit) {
+        return person.getAge() >= ageLimit;
     }
-    
+}
+
+class Person{
+    private String firstName;
+    private String lastName;
+    private String city;
+    private int age;
+
+    Person(String firstName, String lastName, String city, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
