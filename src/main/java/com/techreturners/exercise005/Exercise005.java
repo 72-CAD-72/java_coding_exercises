1de2b2438d07f595923cb0d884c29005cc06870e
cad9wo-max <75642356+cad9wo-max@users.noreply.github.com>
2022-03-15 17:02:13 +0000

    With the lower case ASCII Char checking in the String loop. I removed the redundant regex stripping of punctuation and white spaces.

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -10,24 +10,21 @@ public class Exercise005 {
 
         final int LOWER_ASCII = 96, HIGHER_ASCII = 122;
 
-        /* set to lower case, remove spaces and punctuation */
+        /* set to lower case */
         input = input.toLowerCase(Locale.ROOT);
-        input = input.replaceAll("\\s+","");
-        input = input.replaceAll("\\p{Punct}+", "");
 
         /* put into Hash set to remove duplicates */
         Set<Character> inputString = new HashSet<>();
-        for(int i=0; i < input.length(); i++)
-        {
-            /* extra safe check added to ensure we only collect lower case alphabet chars */
-            if(LOWER_ASCII < input.charAt(i) && input.charAt(i) <= HIGHER_ASCII )
+
+        for (int i = 0; i < input.length(); i++) {
+            /* check to ensure we only collect lower case alphabet chars */
+            if (LOWER_ASCII < input.charAt(i) && input.charAt(i) <= HIGHER_ASCII)
                 inputString.add(input.charAt(i));
         }
 
         /* check length, if equal to length of alphabet then true/ */
         return (inputString.size() >= 26);
-
-        }
     }
+}
 
 
