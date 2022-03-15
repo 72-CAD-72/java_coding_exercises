4edda74fd45e14ab258a059ffe5fb07e05068e1f
cad9wo-max <75642356+cad9wo-max@users.noreply.github.com>
2022-03-15 11:11:52 +0000

    Methods for getIceCream and iceCreamFlavors completed.

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -1,13 +1,31 @@
 package com.techreturners.exercise003;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Exercise003 {
 
+    // simple array -> probably would be passed in extending Exercise003
+    private String[] flavors = {"Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet"};
+
+    // use Map for ease / speed of retrival
+    private Map<String, Integer> flavorMap;
+
+    public Exercise003() {
+
+        flavorMap = new HashMap<String, Integer>();
+
+        for (int i=0;i < flavors.length; i++) {
+            flavorMap.put(flavors[i], i);
+        }
+    }
+
     int getIceCreamCode(String iceCreamFlavour) {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        return flavorMap.getOrDefault(iceCreamFlavour, -1);
     }
 
     String[] iceCreamFlavours() {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        return flavors;
     }
 
 }
