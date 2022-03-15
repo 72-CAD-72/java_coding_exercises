ae3f903628c7a9c6174ad9e78913196dd3f2f790
cad9wo-max <75642356+cad9wo-max@users.noreply.github.com>
2022-03-15 15:25:25 +0000

    Completed Exercise 005 Method Implemented: isPangram()

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -1,9 +1,29 @@
 package com.techreturners.exercise005;
 
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
 public class Exercise005 {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+
+        // set to lower case, remove spaces and punctuation
+        input = input.toLowerCase(Locale.ROOT);
+        input = input.replaceAll("\\s+","");
+        input = input.replaceAll("\\p{Punct}+", "");
+
+        // put into Hash set to remove duplicates
+        Set<Character> inputString = new HashSet<>();
+        for(int i=0; i < input.length(); i++)
+        {
+            inputString.add(input.charAt(i));
+        }
+
+        // check length, if equal to length of alphabet then true/
+        return (inputString.size() >= 26);
+
+        }
     }
 
-}
+
