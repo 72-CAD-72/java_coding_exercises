e012a58f0293932e98c9f6e2bc7969c7526cb60b
cad9wo-max <75642356+cad9wo-max@users.noreply.github.com>
2022-03-15 12:31:12 +0000

    Completed Exercise001 method implementations (capitalizeWord, countLinuxUsers, generateInitials, addVat, reverse )

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -3,28 +3,30 @@ package com.techreturners.exercise001;
 import java.util.List;
 
 public class Exercise001 {
+
     public String capitalizeWord(String word) {
-        // Add your code here
-        return "";
-    }
-
-    public String generateInitials(String firstName, String lastName) {
-        // Add your code here
-        return "";
-    }
-
-    public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
-        return 0.0;
-    }
-
-    public String reverse(String sentence) {
-        // Add your code here
-        return "";
+        return Character.toUpperCase(word.charAt(0)) + word.substring(1);
     }
 
     public int countLinuxUsers(List<User> users) {
-        // Add your code here
-        return 0;
+        long countUsers = users.stream().filter(u -> u.getType() == "Linux").count();
+        return (int)countUsers;
+    }
+
+    public String generateInitials(String firstName, String lastName) {
+        return firstName.substring(0,1) + "." + lastName.substring(0,1);
+    }
+
+    public double addVat(double originalPrice, double vatRate) {
+        return Math.round((originalPrice + (originalPrice * (vatRate/100)))*100.0)/100.0;
+    }
+
+    public String reverse(String sentence) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int i=sentence.length()-1; i >= 0; i--)
+            stringBuilder.append(sentence.charAt(i));
+
+        return stringBuilder.toString();
     }
 }
