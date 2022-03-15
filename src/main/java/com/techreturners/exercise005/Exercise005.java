54ca8c3cdb92a105936fd9232282337953dd007c
cad9wo-max <75642356+cad9wo-max@users.noreply.github.com>
2022-03-15 16:22:47 +0000

    Added an extra safety check to isPangram(): /* extra safe check added to ensure we only collect lower case alphabet chars */

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -8,19 +8,23 @@ public class Exercise005 {
 
     public boolean isPangram(String input) {
 
-        // set to lower case, remove spaces and punctuation
+        final int LOWER_ASCII = 96, HIGHER_ASCII = 122;
+
+        /* set to lower case, remove spaces and punctuation */
         input = input.toLowerCase(Locale.ROOT);
         input = input.replaceAll("\\s+","");
         input = input.replaceAll("\\p{Punct}+", "");
 
-        // put into Hash set to remove duplicates
+        /* put into Hash set to remove duplicates */
         Set<Character> inputString = new HashSet<>();
         for(int i=0; i < input.length(); i++)
         {
-            inputString.add(input.charAt(i));
+            /* extra safe check added to ensure we only collect lower case alphabet chars */
+            if(LOWER_ASCII < input.charAt(i) && input.charAt(i) <= HIGHER_ASCII )
+                inputString.add(input.charAt(i));
         }
 
-        // check length, if equal to length of alphabet then true/
+        /* check length, if equal to length of alphabet then true/ */
         return (inputString.size() >= 26);
 
         }
